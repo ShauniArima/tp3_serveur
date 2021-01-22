8cce4c6e49b2258406e6b17bb0e0bf51a6c642a4
Quentin <quentin.grosmangin@imt-atlantique.com>
2021-01-22 16:42:48 +0100

    RechercheAsynchroneAbstraite

--- src/modele/RechercheAsynchroneAbstraite.java
+++ src/modele/RechercheAsynchroneAbstraite.java
@@ -1,17 +1,34 @@
 package modele;
 
+import java.net.URI;
 import java.util.Optional;
 import java.util.concurrent.Future;
 
 import infrastructure.jaxrs.HyperLien;
+import infrastructure.langage.Types;
+
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.InvocationCallback;
+import javax.ws.rs.client.WebTarget;
+
+import javax.ws.rs.core.UriBuilder;
+
+import configuration.JAXRS;
 
 public abstract class RechercheAsynchroneAbstraite implements AlgorithmeRecherche {
 
-	abstract protected Future<Optional<HyperLien<Livre>>> rechercheAsync(HyperLien<Bibliotheque> h, Livre l, Client client);
+	protected Future<Optional<HyperLien<Livre>>> rechercheAsync(HyperLien<Bibliotheque> h, Livre l, Client client){
+		URI uri = UriBuilder.fromUri(h.getUri()).path(JAXRS.SOUSCHEMIN_ASYNC).build();
+		WebTarget target = client.target(uri);
+		return target.request().async().put(Entity.entity(l, JAXRS.TYPE_MEDIA), Types.typeRetourChercherAsync());
+	};
 
-	abstract protected Future<Optional<HyperLien<Livre>>> rechercheAsyncAvecRappel(
+	protected Future<Optional<HyperLien<Livre>>> rechercheAsyncAvecRappel(
 			HyperLien<Bibliotheque> h, Livre l, Client client,  
-			InvocationCallback<Optional<HyperLien<Livre>>> retour);
+			InvocationCallback<Optional<HyperLien<Livre>>> retour){
+		URI uri = UriBuilder.fromUri(h.getUri()).path(JAXRS.SOUSCHEMIN_ASYNC).build();
+		WebTarget target = client.target(uri);
+		return target.request().async().method("PUT", Entity.entity(l, JAXRS.TYPE_MEDIA), Types.typeRetourChercherAsync());
+	}
 }
